8fe8a883b33d69b552430dba888c471351f37f8f
lllena <lutchencko.lena@gmail.com>
2021-04-20 01:28:23 +0300

    fixed files

---
+++ todo_Redux/src/analytics.js
@@ -0,0 +1,27 @@
+import * as $ from "jquery"
+
+function createAnalytics() {
+    let counter = 0
+    let destroyed = false
+
+    const listener = () => {
+        counter++
+    }
+
+    $(document).on('click', listener)
+    return {
+        destroy() {
+            $(document).of("click", listener)
+            destroyed = true
+        }
+        ,
+        getClicks() {
+            if (destroyed) {
+                return `Analytics is destroyed. Total clicks = ${counter}`
+            }
+            return counter
+        }
+    }
+}
+
+window.analytics = createAnalytics()
