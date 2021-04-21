751b27506f69494435e9eec939d24064edce0d98
lllena <lutchencko.lena@gmail.com>
2021-04-22 02:23:40 +0300

    finished

--- todo-redux/redux/index.js
+++ todo-redux/redux/index.js
@@ -5,32 +5,126 @@ import { execute, completed, all } from './actions';
 class Todo {
   constructor() {
     this.store = createStore(createReducer);
-    this.input = document.querySelector('.input');
+    this.form = document.querySelector('.form');
     this.executeBtn = document.querySelector('.button-execute');
     this.completedBtn = document.querySelector('.button-completed');
     this.allBtn = document.querySelector('.button-all');
-    this.inputField = document.querySelector('.input-field');
+    this.input = document.querySelector('.input');
     this.todoList = document.querySelector('.todo-list');
     this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
     this.state = this.store.getState();
+    this.main = document.querySelector('.main');
   }
+
   addToStorage() {
     localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
   }
-  render() {
-    this.store.subscribe(() => {
-      this.state = this.store.getState();
-    });
-    this.todoList.textContent = '';
-    this.todoData.forEach(this.filterItem.bind(this), this);
-    this.addToStorage();
+
+  generateKey() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   }
-  clearItems() {
-    this.todoData.forEach((item) => {
+
+  itemCheck(completed) {
+    if (completed) return 'check';
+    return 'uncheck';
+  }
+
+  exampleItem() {
+    return {
+      value: this.input.value,
+      completed: false,
+      repeat: false,
+      key: this.generateKey(),
+    };
+  }
+
+  tabItems(item) {
+    switch (this.state) {
+      case 'all':
+        this.createElement(item);
+        break;
+      case 'completed':
+        if (item.completed) this.createElement(item);
+        break;
+      case 'execute':
+        if (!item.completed) this.createElement(item);
+    }
+  }
+
+  handlerAction(bool, func, key = '') {
+    if (bool) {
+      func.call(this, key);
+    }
+  }
+
+  setItem(map, func) {
+    map.forEach((item) => {
+      func.call(this, item);
+    });
+  }
+
+  clear(item) {
+    item.repeat = false;
+  }
+
+  addGreen(item) {
+    if (item.value.match(this.reg) && this.input.value !== '') {
+      this.store.dispatch(all());
+      item.repeat = true;
+    } else {
       item.repeat = false;
+    }
+  }
+
+  deleteItem(key) {
+    this.todoData.forEach((item, index) => {
+      if (item.key !== key) return;
+      this.todoData.delete(index);
     });
   }
-  createElement(todo, check) {
+
+  completedItem(key) {
+    this.todoData.forEach((item) => {
+      if (item.key !== key) return;
+      item.completed = !item.completed;
+    });
+  }
+
+  searchItem(e) {
+    this.reg = new RegExp(e.target.value, true ? 'gi' : '');
+    this.setItem(this.todoData, this.addGreen);
+    this.render();
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    if (!this.input.value.trim()) return;
+    const newTodo = this.exampleItem();
+    this.todoData.set(newTodo.key, newTodo);
+    this.input.value = '';
+    this.setItem(this.todoData, this.clear);
+    this.render();
+  }
+
+  init() {
+    todo.render();
+    this.form.addEventListener('input', this.searchItem.bind(this));
+    this.form.addEventListener('submit', this.addTodo.bind(this));
+  }
+
+  handler() {
+    this.main.addEventListener('click', ({ target }) => {
+      const key = target.closest('.todo-item')?.getAttribute('data-id');
+      this.handlerAction(target.classList.value.split(' ')['0'] === 'todo-complete', this.completedItem, key);
+      this.handlerAction(target.classList.value === 'todo-remove', this.deleteItem, key);
+      this.handlerAction(target === this.executeBtn, this.store.dispatch, execute());
+      this.handlerAction(target === this.completedBtn, this.store.dispatch, completed());
+      this.handlerAction(target === this.allBtn, this.store.dispatch, all());
+      this.render();
+    });
+  }
+
+  createElement(todo) {
     const li = document.createElement('li');
     li.setAttribute('data-id', todo.key);
     li.classList.add('todo-item');
@@ -40,118 +134,20 @@ class Todo {
       `<span class="text-todo">${todo.value}</span>
 		<div class="todo-buttons">
             <button class="todo-remove"></button>
-            <button class="todo-complete ${check}"></button>
+            <button class="todo-complete ${this.itemCheck(todo.completed)}"></button>
         </div>`,
     );
     this.todoList.append(li);
   }
 
-  filterItem(todo) {
-    let check;
-    if (todo.completed == true) {
-      check = 'check';
-    } else {
-      check = 'uncheck';
-    }
-    if (this.state === 'all') {
-      this.createElement(todo, check);
-    } else if (this.state === 'completed') {
-      if (todo.completed === true) {
-        this.createElement(todo, check);
-      }
-    } else if (this.state === 'execute') {
-      if (todo.completed === false) {
-        this.createElement(todo, check);
-      }
-    }
-  }
-  addTodo(e) {
-    e.preventDefault();
-    if (this.inputField.value.trim()) {
-      const newTodo = {
-        value: this.inputField.value,
-        completed: false,
-        repeat: false,
-        key: this.generateKey(),
-      };
-      this.todoData.set(newTodo.key, newTodo);
-      this.inputField.value = '';
-      this.todoData.forEach((item) => {
-        item.repeat = false;
-      });
-      this.render();
-    }
-  }
-  searchItem(e) {
-    const reg = new RegExp(e.target.value, true ? 'gi' : '');
-    this.todoData.forEach((item) => {
-      if (item.value.match(reg) && this.inputField.value != '') {
-        this.store.dispatch(all());
-        item.repeat = true;
-        this.render();
-      } else {
-        item.repeat = false;
-        this.render();
-      }
-    });
-  }
-  generateKey() {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  }
-  deleteItem(key) {
-    this.todoData.forEach((item, index) => {
-      if (item.key === key) {
-        this.todoData.delete(index);
-      }
-      this.render();
-    });
-  }
-  completedItem(key) {
-    this.todoData.forEach((item) => {
-      if (item.key === key) {
-        if (item.completed === false) {
-          item.completed = true;
-        } else if (item.completed === true) {
-          item.completed = false;
-        }
-      }
-      this.render();
-    });
-  }
-
-  handler() {
-    let main = document.querySelector('.main');
-    main.addEventListener('click', ({ target }) => {
-      const key = target.closest('.todo-item')?.getAttribute('data-id');
-      if (target.classList.value.split(' ')['0'] === 'todo-complete') {
-        this.completedItem(key);
-      }
-      if (target.classList.value === 'todo-remove') {
-        this.deleteItem(key);
-      }
-      if (target === this.executeBtn) {
-        this.store.dispatch(execute());
-        this.render();
-      }
-      if (target === this.completedBtn) {
-        this.store.dispatch(completed());
-        this.render();
-      }
-      if (target === this.allBtn) {
-        this.store.dispatch(all());
-        this.render();
-      }
-    });
-  }
-
-  init() {
-    this.input.addEventListener('submit', this.addTodo.bind(this));
-    this.render();
-    this.input.addEventListener('input', this.searchItem.bind(this));
+  render() {
+    this.store.subscribe(() => (this.state = this.store.getState()));
+    this.todoList.textContent = '';
+    this.todoData.forEach(this.tabItems.bind(this), this);
+    this.addToStorage();
   }
 }
 
 const todo = new Todo();
 todo.init();
 todo.handler();
-todo.store.dispatch({ type: 'all' });
