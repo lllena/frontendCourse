9ae7a77251a2c8a32b39e73c972bbe18f1016164
lllena <lutchencko.lena@gmail.com>
2021-04-27 01:36:44 +0300

    fixed code

--- slider/slider-plagin.js
+++ slider/slider-plagin.js
@@ -1,57 +1,38 @@
 class Slider {
-  constructor(
-    { width = 500, height = 400 },
-    {
-      infinite = true,
-      slidesToShow = 1,
-      slidesToScrool = 1,
-      autoplay = true,
-      dots = true,
-      pauseOnHover = true,
-      pauseOnDotsHover = true,
-      speed = 500,
-      autoplaySpeed = 1500,
-    }
-  ) {
-    this.infinite = infinite;
+  constructor({
+    slidesToShow = 1,
+    slidesToScrool = 1,
+    autoplay = true,
+    pauseOnHover = true,
+    speed = 500,
+    autoplaySpeed = 1500,
+  }) {
     this.slidesToShow = slidesToShow;
     this.slidesToScrool = slidesToScrool;
     this.autoplay = autoplay;
-    this.dots = dots;
     this.pauseOnHover = pauseOnHover;
-    this.pauseOnDotsHover = pauseOnDotsHover;
     this.speed = speed;
     this.autoplaySpeed = autoplaySpeed;
     this.interval = 0;
     this.position = 0;
     this.maxSlides = Math.ceil(100 / this.slidesToShow);
-    this.style = {
-      width,
-      height,
-    };
     this.wrap = document.querySelector(".slider-wrapper");
     this.slides = document.querySelector(".slider-wrapper").children;
     this.prev = document.querySelector(".slider-arrow--prev");
     this.next = document.querySelector(".slider-arrow--next");
     this.container = document.querySelector(".slider-container");
   }
+
   addStyle() {
     const style = document.createElement("style");
     style.id = "slider-style";
     style.textContent = `
-        .slider-container {
-            width: ${this.style.width}px;
-            height: ${this.style.height}px;
-        }
         .slider-item {
             flex: 0 0 ${this.maxSlides}%;
         }
         .slider-item {
             opacity: ${this.fade};
         }
-        .slider-arrow--next{
-         margin: ${this.style.width - 25}px;
-            }
         .slider-wrapper {
             transition: transform ${this.speed}s;
           }
@@ -62,12 +43,12 @@ class Slider {
   prevSlide() {
     if (this.position > 0) {
       --this.position;
-      console.log("prev");
       this.wrap.style.transform = `translateX(-${
         this.position * this.maxSlides * this.slidesToScrool
       }%)`;
     }
   }
+
   nextSlide() {
     if (this.position < this.slides.length - this.slidesToShow) {
       ++this.position;
@@ -76,18 +57,22 @@ class Slider {
       }%)`;
     }
   }
+
   startSlide() {
     this.interval = setInterval(
       this.autoPlaySlide.bind(this),
       this.autoplaySpeed
     );
   }
+
   stopSlide() {
     clearInterval(this.interval);
   }
+
   autoPlaySlide() {
     this.nextSlide();
   }
+
   mouseOver(e) {
     if (
       e.target.matches(".slider-arrow") ||
@@ -96,6 +81,7 @@ class Slider {
       this.stopSlide();
     }
   }
+
   mouseOut(e) {
     if (
       e.target.matches(".slider-arrow") ||
