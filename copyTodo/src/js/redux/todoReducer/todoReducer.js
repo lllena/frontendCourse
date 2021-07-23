563fe05860b8b0325ea0bd64f5e231f1d9cf0ed8
lllena <lutchencko.lena@gmail.com>
2021-07-23 15:32:37 +0300

    add old files

---
+++ copyTodo/src/js/redux/todoReducer/todoReducer.js
@@ -0,0 +1,26 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_PARAM_TODO } from './types';
+
+export function todoReducer(state = [], { type, payload }) {
+  switch (type) {
+    case ADD_TODO:
+      return [...state, payload.data];
+
+    case REMOVE_TODO:
+      return [...state.filter((todo) => todo.id !== payload.id)];
+
+    case TOGGLE_PARAM_TODO:
+      return [
+        ...state.map((todo) => {
+          if (todo.id === payload.id) {
+            return Object.assign({}, todo, {
+              [payload.param]: !todo[payload.param],
+            });
+          } else {
+            return todo;
+          }
+        }),
+      ];
+  }
+
+  return state;
+}
