a39064558c9eb9b14bcc65016436d68b187cb9ca
admin <lutchencko.lena@gmail.com>
2021-08-05 14:29:51 +0300

    deleted copy

--- todo-redux/redux/types.js
+++
@@ -1,8 +0,0 @@
-export const EXECUTE_TASK = 'execute';
-export const COMPLETED_TASK = 'completed';
-export const ALL_TASK = 'all';
-
-export const ADD = 'add';
-export const REMOVE = 'remove';
-export const TOGGLE_COMPLETE = 'toggleComplete';
-export const SEARCH = 'search';
